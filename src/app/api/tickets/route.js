b27be50f268aba5e545f1bb2cef631226fe63964
Vikas Kumar <80447331+vikasOzark@users.noreply.github.com>
2023-10-31 22:45:11 +0530

    new code with get routes fixed

--- src/app/api/tickets/route.js
+++ src/app/api/tickets/route.js
@@ -1,3 +1,4 @@
+import httpStatus from "@/utils/httpStatus";
 import getUserId from "@/utils/userByToken";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
@@ -7,7 +8,7 @@ export async function POST(request) {
   const requestBody = await request.json();
   try {
     const userId = await getUserId(request);
-    prisma.$connect()
+    prisma.$connect();
     const ticket = await prisma.tickets.create({
       data: {
         taskTitle: requestBody.taskTitle,
@@ -31,7 +32,6 @@ export async function POST(request) {
       message: "Ticket is created successfully.",
       data: [],
     });
-
   } catch (error) {
     console.log(error.message);
     let message = null;
@@ -49,38 +49,75 @@ export async function POST(request) {
   }
 }
 
+// export async function GET(request) {
+//   const requestBody = await request.json();
+//   console.log(requestBody);
+//   try {
+//     const userId = await getUserId(request);
+//     // prisma.$connect()
+
+//     // const allTicket = await prisma.tickets.findMany({
+//     //     where : {
+//     //         userId : userId
+//     //     }
+//     // })
+
+//     return NextResponse.json({
+//       success: true,
+//       message: "Ticket is created successfully.",
+//       data: allTicket,
+//     });
+//   } catch (error) {
+//     let message = null;
+
+//     if (error.message.split(":")[0] === "self") {
+//       message = error.message;
+//     } else {
+//       message = "Something went wrong.";
+//     }
+
+//     return NextResponse.json({
+//       success: false,
+//       message: message,
+//       data: [],
+//     });
+//   }
+// }
+
 export async function GET(request) {
-  const requestBody = await request.json();
-  console.log(requestBody);
   try {
-    const userId = await getUserId(request);
-    prisma.$connect()
+    const userObjectId = await getUserId(request);
+    prisma.$connect();
 
-    const allTicket = await prisma.tickets.findMany({
-        where : {
-            userId : userId
-        }
-    })
+    // await prisma.tickets.findMany
 
-    return NextResponse.json({
-      success: true,
-      message: "Ticket is created successfully.",
-      data: allTicket,
+    const ticketsData = await prisma.tickets.findMany({
+      where: {
+        userId: userObjectId,
+      },
+      include: {
+        department: true,
+        where: userObjectId,
+      },
     });
 
+    return NextResponse.json(
+      {
+        message: "Successfully get the department data.",
+        success: true,
+        data: ticketsData,
+      },
+      { status: httpStatus.HTTP_200_OK }
+    );
   } catch (error) {
-    let message = null;
-    
-    if (error.message.split(":")[0] === "self") {
-      message = error.message;
-    } else {
-      message = "Something went wrong.";
-    }
-
-    return NextResponse.json({
-      success: false,
-      message: message,
-      data: [],
-    });
+    console.log(error.message);
+    return NextResponse.json(
+      {
+        message: "Internal server error, Please try again.",
+        success: false,
+        data: [],
+      },
+      { status: httpStatus.HTTP_500_INTERNAL_SERVER_ERROR }
+    );
   }
 }
--- src/app/dashboard/tickets/components.jsx
+++ src/app/dashboard/tickets/components.jsx
@@ -40,14 +40,29 @@ export const TableRow = () => {
     "Edit",
   ];
 
-    const responseDatas = useQuery("tickets-list", async () => {
+  const responseData = useQuery("tickets-list", async () => {
     const response = await fetch("/api/tickets");
     const json_response = await response.json();
-    console.log(json_response);
     return json_response;
   });
 
-  console.log(responseDatas);
+  // {
+  //   id: '6540cc080f3f527f2d0ab80e',
+  //   taskTitle: 'as',
+  //   ticketDetil: 'asa',
+  //   status: 'PENDING',
+  //   departmentId: '653ceb8d85741601a05f2b04',
+  //   userId: '653750505572dfd718166890',
+  //   tagId: null,
+  //   createdAt: '2023-10-31T09:42:32.296Z',
+  //   department: {
+  //     id: '653ceb8d85741601a05f2b04',
+  //     name: 'etsting',
+  //     userId: '653750505572dfd718166890',
+  //     updated_at: '2023-10-28T11:07:56.954Z',
+  //     createdAt: '2023-10-28T11:07:56.954Z'
+  //   }
+  // },
 
   return (
     <>
@@ -74,7 +89,7 @@ export const TableRow = () => {
                     scope="col"
                     className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                   >
-                    Status
+                    Ticket status
                   </th>
 
                   <th
@@ -88,23 +103,16 @@ export const TableRow = () => {
                     scope="col"
                     className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                   >
-                    Assigned to
-                  </th>
-
-                  <th
-                    scope="col"
-                    className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                  >
-                    Teams
-                  </th>
-
-                  <th scope="col" className="relative py-3.5 px-4">
-                    <span className="sr-only">Edit</span>
+                    Action
                   </th>
                 </tr>
               </thead>
+
               <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                <TableBodyRow />
+                {responseData.isSuccess &&
+                  responseData.data.data?.map((ticket) => (
+                    <TableBodyRow key={ticket.id} data={ticket} />
+                  ))}
               </tbody>
             </table>
           </div>
@@ -114,83 +122,75 @@ export const TableRow = () => {
   );
 };
 
-const TableBodyRow = () => {
+const TableBodyRow = ({ data }) => {
   return (
-    <>
-      <tr>
-        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-          <div className="inline-flex items-center gap-x-3">
-            <input
-              type="checkbox"
-              className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700"
-            />
+    <tr>
+      <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+        <div className="inline-flex items-center gap-x-3">
+          <input
+            type="checkbox"
+            className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700"
+          />
 
-            <div className="flex items-center gap-x-2">
-              <div>
-                <h2 className="font-medium text-gray-800 dark:text-white ">
-                  It solution issue
-                </h2>
-              </div>
+          <div className="flex items-center gap-x-2">
+            <div>
+              <h2 className="font-medium text-gray-800 dark:text-white ">
+                {data.taskTitle}
+              </h2>
             </div>
           </div>
-        </td>
-        <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-          <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
-            <span className="h-1.5 w-1.5 rounded-full bg-emerald-500"></span>
+        </div>
+      </td>
 
-            <h2 className="text-sm font-normal text-emerald-500">Active</h2>
-          </div>
-        </td>
-        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-          Graphic Designer
-        </td>
-        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-          mia@example.com
-        </td>
-        <td className="px-4 py-4 text-sm whitespace-nowrap">
-          <div className="flex items-center gap-x-2">
-            <TaskTag />
-          </div>
-        </td>
-        <td className="px-4 py-4 text-sm whitespace-nowrap">
-          <div className="flex items-center gap-x-6">
-            <button className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-            </button>
+      <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+        <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-emerald-300 dark:bg-gray-800">
+          <span className="h-1.5 w-1.5 rounded-full bg-emerald-600"></span>
+          <h2 className="text-sm font-normal text-white">Active</h2>
+        </div>
+      </td>
 
-            <button className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                />
-              </svg>
-            </button>
-          </div>
-        </td>
-      </tr>
-    </>
+      <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+        {data.department.name}
+      </td>
+
+      <td className="px-4 py-4 text-sm whitespace-nowrap">
+        <div className="flex items-center gap-x-6">
+          <button className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              />
+            </svg>
+          </button>
+
+          <button className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+              />
+            </svg>
+          </button>
+        </div>
+      </td>
+    </tr>
   );
 };
 
